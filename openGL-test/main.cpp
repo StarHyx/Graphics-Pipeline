ed84a3bf650ed649813e23cf3539ab4155826e77
StarHyx <450479592@qq.com>
2017-11-20 19:03:47 +0800

    Initial Commit

---
+++ openGL-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openGL-test
+//
+//  Created by Han Yixing on 2017/11/20.
+//  Copyright © 2017年 Han Yixing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
